a182469479aedeae3a25f83efef35a666b9cecae
badmitry <dmitrij88@bk.ru>
2020-11-26 10:50:38 +0300

    rxJava The beginning

--- app/src/main/java/com/badmitry/github/mvp/model/repo/GithubUserRepo.kt
+++ app/src/main/java/com/badmitry/github/mvp/model/repo/GithubUserRepo.kt
@@ -1,17 +1,48 @@
 package com.badmitry.github.mvp.model.repo
 
+import android.util.Log
 import com.badmitry.github.mvp.model.entity.GithubUser
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 class GithubUsersRepo {
-    private val repositories = listOf(
-        GithubUser("login1"),
-        GithubUser("login2"),
-        GithubUser("login3"),
-        GithubUser("login4"),
-        GithubUser("login5")
-    )
+    private var repositories = mutableListOf<GithubUser>()
+    private var newRepo = mutableListOf<GithubUser>()
+    var counter = 0
 
-    fun getUsers() : List<GithubUser> {
-        return repositories
+    private fun downloadUsers(): MutableList<GithubUser> {
+        return mutableListOf(
+            GithubUser("login1"),
+            GithubUser("login2"),
+            GithubUser("login3"),
+            GithubUser("login4"),
+            GithubUser("login5")
+        )
+    }
+
+    fun create() = Observable.create<MutableList<GithubUser>> { emitter ->
+        while (true) {
+            counter++
+            newRepo = downloadUsers()
+            if (counter == 5) {
+                newRepo.add(GithubUser("login6"))
+            }
+            if (!newRepo.equals(repositories)) {
+                repositories = newRepo
+                Log.d("!!!", "downloadUsers: " + false)
+                emitter.onNext(repositories)
+                if (counter == 5) {
+                    emitter.onComplete()
+                    break
+                }
+            } else {
+                Log.d("!!!", "downloadUsers: " + true)
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
--- app/src/main/java/com/badmitry/github/mvp/presenter/UsersPresenter.kt
+++ app/src/main/java/com/badmitry/github/mvp/presenter/UsersPresenter.kt
@@ -1,18 +1,21 @@
 package com.badmitry.github.mvp.presenter
 
+import android.util.Log
 import com.badmitry.github.mvp.model.entity.GithubUser
 import com.badmitry.github.mvp.model.repo.GithubUsersRepo
 import com.badmitry.github.mvp.presenter.list.IUserListPresenter
 import com.badmitry.github.mvp.view.IUsersView
 import com.badmitry.github.mvp.view.list.IUserItemView
 import com.badmitry.github.navigation.Screens
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
 
 class UsersPresenter(private val usersRepo: GithubUsersRepo, private val router: Router) :
     MvpPresenter<IUsersView>() {
     class UserListPresenter : IUserListPresenter {
-        val users = mutableListOf<GithubUser>()
+        var users = mutableListOf<GithubUser>()
         override var itemClickListener: ((IUserItemView) -> Unit)? = null
         override fun getCount() = users.size
         override fun bindView(view: IUserItemView) {
@@ -26,15 +29,18 @@ class UsersPresenter(private val usersRepo: GithubUsersRepo, private val router:
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         viewState.init()
-        loadData()
+        usersRepo.create().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).
+        subscribe() {
+            loadData(it)
+            Log.d("!!!", "onFirstViewAttach: ")
+        }
         userListPresenter.itemClickListener = {itemView ->
             router.navigateTo(Screens.UserScreen(userListPresenter.users[itemView.pos]))
         }
     }
 
-    private fun loadData() {
-        val users = usersRepo.getUsers()
-        userListPresenter.users.addAll(users)
+    private fun loadData(users: MutableList<GithubUser>) {
+        userListPresenter.users = users
         viewState.updateList()
     }
 
